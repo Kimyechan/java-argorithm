8a02ec7d4289e372cd16fdd7787081662f3cc36a
Kimyechan <vlvkcjswo7@naver.com>
2021-07-08 00:57:00 +0900

    Solve: baekjun 피보나치 10870

---
+++ src/com/company/baekjun/b10870/Main.java
@@ -0,0 +1,29 @@
+package com.company.baekjun.b10870;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int result = fibonacci(n);
+
+        System.out.println(result);
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 2) + fibonacci(n - 1);
+        }
+    }
+}
