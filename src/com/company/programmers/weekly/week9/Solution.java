f368a0b2978a6956b0af87c0e8237cdccfe75b5a
Kimyechan <vlvkcjswo7@naver.com>
2021-10-28 22:29:38 +0900

    Solve: programmers 전력망을 둘로 나누기

---
+++ src/com/company/programmers/weekly/week9/Solution.java
@@ -0,0 +1,69 @@
+package com.company.programmers.weekly.week9;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] wires) {
+        int answer = Integer.MAX_VALUE;
+
+        for (int i = 0; i < wires.length; i++) {
+            Map<Integer, List<Integer>> map = new HashMap<>();
+            for (int j = 1; j <= n; j++) {
+                map.put(j, new ArrayList<>());
+            }
+            for (int j = 0; j < wires.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                map.get(wires[j][0]).add(wires[j][1]);
+                map.get(wires[j][1]).add(wires[j][0]);
+            }
+            List<Integer> sizeList = new ArrayList<>();
+            boolean[] visited = new boolean[n + 1];
+            for (int j = 1; j <= n; j++) {
+                if (visited[j]) {
+                    continue;
+                }
+
+                int count = bfs(j, visited, map);
+                sizeList.add(count);
+            }
+
+            answer = Math.min(answer, Math.abs(sizeList.get(0) - sizeList.get(1)));
+        }
+
+        return answer;
+    }
+
+    public int bfs(int start, boolean[] visited, Map<Integer, List<Integer>> map) {
+        int count = 0;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            Integer node = q.poll();
+
+            count += 1;
+
+            for (Integer num : map.get(node)) {
+                if (visited[num]) {
+                    continue;
+                }
+
+                visited[num] = true;
+                q.offer(num);
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(9,	new int[][]{{1,3},{2,3},{3,4},{4,5},{4,6},{4,7},{7,8},{7,9}}));
+    }
+}
+
