82a77729a161f2eaf9183728f17b9fcba9c1e182
Kimyechan <vlvkcjswo7@naver.com>
2021-11-08 22:56:29 +0900

    Solve: leetcode intersection of two arrays ii

---
+++ src/com/company/leetcode/intersection_of_two_arrays_ii/Solution.java
@@ -0,0 +1,28 @@
+package com.company.leetcode.intersection_of_two_arrays_ii;
+
+import java.util.*;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        for (int n : nums1) {
+            countMap.put(n, countMap.getOrDefault(n, 0) + 1);
+        }
+
+        List<Integer> list = new ArrayList();
+        for (int n : nums2) {
+            if (countMap.containsKey(n) && countMap.get(n) != 0) {
+                countMap.put(n, countMap.get(n) - 1);
+                list.add(n);
+            }
+        }
+
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+}
