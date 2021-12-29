770a354888b79b217970c5f3234a11f239be7aaa
Kimyechan <vlvkcjswo7@naver.com>
2021-12-29 14:57:38 +0900

    Check: leetcode container with most water

---
+++ src/com/company/leetcode/container_with_most_water/Solution.java
@@ -0,0 +1,22 @@
+package com.company.leetcode.container_with_most_water;
+
+class Solution {
+    public int maxArea(int[] height) {
+        int maxWater = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            int water = Math.min(height[left], height[right]) * (right - left);
+            maxWater = Math.max(maxWater, water);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+}
