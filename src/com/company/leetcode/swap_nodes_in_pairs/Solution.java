057006c098dbd7ce647f35b160db6cfcffede4a1
Kimyechan <vlvkcjswo7@naver.com>
2022-01-07 15:18:25 +0900

    Check: leetcode swap nodes in pairs

---
+++ src/com/company/leetcode/swap_nodes_in_pairs/Solution.java
@@ -0,0 +1,29 @@
+package com.company.leetcode.swap_nodes_in_pairs;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if ((head == null) || (head.next == null))
+            return head;
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
+}
