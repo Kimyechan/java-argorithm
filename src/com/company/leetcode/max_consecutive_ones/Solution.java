be596ade4f574bd1db7cd3da707d23ee9cbf1728
Kimyechan <vlvkcjswo7@naver.com>
2022-01-10 20:57:04 +0900

    Solve: max consecutive ones

---
+++ src/com/company/leetcode/max_consecutive_ones/Solution.java
@@ -0,0 +1,18 @@
+package com.company.leetcode.max_consecutive_ones;
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int result = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+                result = Math.max(result, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        return result;
+    }
+}
